0ab45654c268344fab44765805421f8d1a1e614b
ryan-907 <ryan.thornton@lander.edu>
2025-11-24 16:55:41 -0600

    debug prints

--- src/image_gen.py
+++ src/image_gen.py
@@ -4,7 +4,7 @@ from diffusers import DiffusionPipeline
 # switch to "mps" for apple devices
 
 def generate_image(animal: str, model: str ="stabilityai/stable-diffusion-xl-base-1.0"):
-    pipe = DiffusionPipeline.from_pretrained(model, dtype=torch.bfloat16, device_map="cuda")
+    pipe = DiffusionPipeline.from_pretrained(model=model, dtype=torch.bfloat16, device_map="auto")
     prompt = f"Photo realistic depiction of {animal}"
     image = pipe(prompt).image[0]
     return image
